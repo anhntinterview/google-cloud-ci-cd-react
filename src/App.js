1881ae6f2ebc6e8525a4ff9fed51867113a6c2d7
anhntinteview <roberthost.vn@gmail.com>
2023-03-05 17:50:47 +0700

    update circleci config.yml4

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello World
+          {/* Learn React */}
         </a>
       </header>
     </div>
